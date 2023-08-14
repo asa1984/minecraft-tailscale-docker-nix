95e7874fcb580dd876b8a6fc2b2777bcee908e0a
asa1984 <satoasa9913@gmail.com>
2023-08-14 18:06:52 +0900

    improve: reduce image size
    commit 96b7a77d3032718b8462b39461e93a82a0f032f3
    Author: asa1984 <satoasa9913@gmail.com>
    Date:   Mon Aug 14 18:04:29 2023 +0900
    
        change: jre -> jdk20_headless

--- flake.nix
+++ flake.nix
@@ -13,30 +13,29 @@
         pkgs = import nixpkgs {
           inherit system;
         };
+        papermc = pkgs.papermc.overrideAttrs {
+          buildPhase = ''
+            cat > minecraft-server << EOF
+            #!${pkgs.bash}/bin/sh
+            exec ${pkgs.jdk20_headless}/bin/java \$@ -jar $out/share/papermc/papermc.jar nogui
+          '';
+        };
         startup = ''
-          /bin/tailscaled --tun=userspace-networking --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock --socks5-server=localhost:1080 &
-          /bin/tailscale up --authkey=$TAILSCALE_AUTH_KEY --hostname=$TAILSCALE_HOSTNAME
-          /bin/minecraft-server -DsocksProxyHost=localhost -DsocksProxyPort=1080
+          ${pkgs.tailscale}/bin/tailscaled --tun=userspace-networking --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock --socks5-server=localhost:1080 &
+          ${pkgs.tailscale}/bin/tailscale up --authkey=$TAILSCALE_AUTH_KEY --hostname=$TAILSCALE_HOSTNAME
+          ${papermc}/bin/minecraft-server -DsocksProxyHost=localhost -DsocksProxyPort=1080
         '';
       in {
         packages = rec {
           minecraft-tailscale-docker-nix = pkgs.dockerTools.buildImage {
             name = "minecraft-tailscale-docker-nix";
             tag = "latest";
-            copyToRoot = pkgs.buildEnv {
-              name = "image-root";
-              paths = with pkgs; [
-                tailscale
-                papermc
-              ];
-              pathsToLink = ["/bin"];
-            };
             runAsRoot = ''
               mkdir -p /var/run/tailscale /var/lib/tailscale /var/cache/tailscale
             '';
             config = {
               Cmd = [
-                "${pkgs.bash}/bin/bash"
+                "${pkgs.bash}/bin/sh"
                 "-c"
                 startup
               ];
